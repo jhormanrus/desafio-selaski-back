0c3239a1ad4f3d8a86e27c9e0bedcb62c2203b5c
Jhorman Rus <jhormanrus@gmail.com>
2022-05-03 10:26:23 -0500

    add cors and env port

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const port = 3000
 
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send({hello: 'Hello World!'})
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App listening on port ${port}!`)
 })
